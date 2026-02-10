43ec0bedb0c25445f10a5a7e8007fbcd66281135
Ceri Bain <cerimac@Ceris-MacBook-Pro-3.local>
2026-02-10 14:39:04 +0000

    updated connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,6 @@
 const { Pool } = require("pg");
 
 const ENV = process.env.NODE_ENV || "development";
-const config = {};
-if (ENV === "production") {
-  config.connectionString = process.env.DATABASE_URL;
-  config.max = 2;
-}
 require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
 
 // const db = new Pool();
@@ -16,5 +11,11 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   console.log(`Connected to ${process.env.PGDATABASE}`);
 }
 
+const config = {};
+
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
 // module.exports = db;
 module.exports = new Pool(config);
